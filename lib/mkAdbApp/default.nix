f00847a06089cef6799cb23bb967274f6a05a811
Dominic Shelton <frogamic@protonmail.com>
2021-06-27 18:12:24 +1000

    Use writeShellScriptBin properly

--- lib/mkAdbApp/default.nix
+++ lib/mkAdbApp/default.nix
@@ -1,10 +1,9 @@
-{ stdenv, scrcpy, writeScriptBin }: { bin, name ? "", crop ? "", title ? "" }: writeScriptBin "${bin}" ''
-	#! ${stdenv.shell}
-		${if name != "" then ''
-			adb shell monkey -p '${name}' \
-				-c android.intent.category.LAUNCHER 1 \
-				> /dev/null
-		'' else ""}
+{ stdenv, scrcpy, writeShellScriptBin }: { bin, name ? "", crop ? "", title ? "" }: writeShellScriptBin "${bin}" ''
+	${if name != "" then ''
+		adb shell monkey -p '${name}' \
+			-c android.intent.category.LAUNCHER 1 \
+			> /dev/null
+	'' else ""}
 	exec ${scrcpy}/bin/scrcpy -Sw \
 		--window-title '${if title == "" then bin else title}' \
 		${if crop != "" then "--crop '${crop}'" else ""} \
